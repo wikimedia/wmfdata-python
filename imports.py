79612e29dfbf8183fee9bb2966d573f7f7fe240b
Neil Patel Quinn <nquinn@wikimedia.org>
2018-04-25 00:37:33 +0000

    Update imports

--- imports.py
+++ imports.py
@@ -1,5 +1,6 @@
 # Standard imports for analysis projects
 import numpy as np
 import pandas as pd
-import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import seaborn as sns
