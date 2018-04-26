727891a8d83f489f26e73acefa6e7bd25d8505a2
Neil Patel Quinn <nquinn@wikimedia.org>
2018-04-26 01:30:13 +0000

    Remove imports of other packages

--- __init__.py
+++ __init__.py
@@ -2,12 +2,5 @@
 from . import query
 from . import utils
 
-# Standard imports for analysis projects
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 # Direct reusers to package
 utils.print_err("You can find the source for `wmfdata` at https://github.com/neilpquinn/wmfdata.")
