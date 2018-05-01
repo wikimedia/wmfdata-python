72a383ae16c18e9e229f72131ee8fe3c8a21ff1a
Neil Patel Quinn <nquinn@wikimedia.org>
2018-05-01 01:21:19 +0000

    Remove trailing period which messed up URL display

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ from . import mariadb
 from . import utils
 
 # Direct reusers to package
-utils.print_err("You can find the source for `wmfdata` at https://github.com/neilpquinn/wmfdata.")
+utils.print_err("You can find the source for `wmfdata` at https://github.com/neilpquinn/wmfdata")
