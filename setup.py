6c662dfb482c5d71cc5efbf98eecf639a17958e9
Neil Patel Quinn <nquinn@wikimedia.org>
2018-05-17 19:06:05 +0000

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wmfdata",
+    version="0.1",
+    description="Tools for analyzing data on SWAP, a platform for confidential Wikimedia data",
+    install_requires=["pandas", "impyla", "mysql-connector-python"],
+    python_requires=">=3"
+)
