443310490f884149ff2f05230cb4d87c57db1a95
Neil Patel Quinn <nquinn@wikimedia.org>
2018-05-18 23:26:52 +0000

    Allow hive.run() to accept multiple commands

--- hive.py
+++ hive.py
@@ -1,28 +1,32 @@
 from impala.dbapi import connect as impala_conn
 from impala.util import as_pandas as impala_as_pd
             
-# To-do: allow for multiple commands as with `mariadb.run()`
-def run(cmd, fmt = "pandas"):
+def run(cmds, fmt = "pandas"):
     """Used to run a Hive query or command on the Data Lake stored on the Analytics cluster."""
     
     if fmt not in ["pandas", "raw"]:
         raise ValueError("The format should be either `pandas` or `raw`.")
     
-    result = None
+    if type(cmds) == str:
+        cmds = [cmds]
+    
+    results = []
     
     try:
         hive_conn = impala_conn(host='analytics1003.eqiad.wmnet', port=10000, auth_mechanism='PLAIN')
         hive_cursor = hive_conn.cursor()
-        hive_cursor.execute(cmd)
-        if fmt == "pandas":
-            try:
-                result = impala_as_pd(hive_cursor)
-            # Happens if there are no results (as with an INSERT INTO query)
-            except TypeError:
-                pass
-        else:
-            result = hive_cursor.fetchall()    
+        
+        for cmd in cmds:
+            hive_cursor.execute(cmd)
+            if fmt == "pandas":
+                try:
+                    results.append(impala_as_pd(hive_cursor))
+                # Happens if there are no results (as with an INSERT INTO query)
+                except TypeError:
+                    results.append(None)
+            else:
+                results.append(hive_cursor.fetchall()) 
     finally:
         hive_conn.close()
     
-    return result
+    return results
