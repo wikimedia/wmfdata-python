02c5d707c24d65c768c527dc305afe70c19c6706
Neil Patel Quinn <nquinn@wikimedia.org>
2018-11-14 02:49:49 +0000

    Add upload_csv function

--- wmfdata/hive.py
+++ wmfdata/hive.py
@@ -1,5 +1,10 @@
+import datetime as dt
+from shutil import copyfileobj
+import subprocess
+
 from impala.dbapi import connect as impala_conn
 from impala.util import as_pandas as impala_as_pd
+from wmfdata.utils import print_err, mediawiki_dt
             
 def run(cmds, fmt = "pandas"):
     """
@@ -34,3 +39,72 @@ def run(cmds, fmt = "pandas"):
         hive_conn.close()
     
     return result
+
+def upload_csv(
+    path, field_spec, db_name, table_name,
+    create_db=False, sep=",", headers=True
+):
+    """
+    Upload a CSV (or other delimiter-separated value file) to Data Lake's HDFS, for use with Hive
+    and other utilities. 
+    
+    `field_spec` specifies the field names and their formats, for the
+    `CREATE TABLE` statement; for example, `name string, age int, graduated bool`.
+    
+    To prevent errors caused by typos, the function will not try to create the database first unless
+    `create_db=True` is passed. 
+    
+    `headers` gives whether the file has a header row; if it does, the function strips it before
+    uploading, because Hive treats all rows as data rows.
+    """
+    if headers:
+        new_path = "/tmp/wmfdata-" + mediawiki_dt(dt.datetime.now())
+        # From rbtsbg at https://stackoverflow.com/a/39791546 
+        with open(path, 'r') as source, open(new_path, 'w') as target:
+            source.readline()
+            copyfileobj(source, target)
+        
+        path = new_path
+    
+    create_db_cmd = """
+    create database if not exists {db_name}
+    """.format(
+        db_name=db_name
+    )
+    
+    create_table_cmd = """
+    create table if not exists {db_name}.{table_name} ({field_spec})
+    row format delimited fields terminated by "{sep}"
+    """.format(
+        db_name=db_name, table_name=table_name,
+        field_spec=field_spec, sep=sep
+    )
+    
+    load_table_cmd = """
+        load data local inpath "{path}"
+        overwrite into table {db_name}.{table_name}
+    """.format(
+        path=path, db_name=db_name,
+        table_name=table_name
+    )
+
+    if create_db:
+        run(create_db_cmd)
+    
+    run(create_table_cmd)
+            
+    proc = subprocess.Popen(
+        ["hive", "-e", load_table_cmd], 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.STDOUT
+    )
+
+    try:
+        outs, _ = proc.communicate(timeout=15)
+        for line in outs.decode().split("\n"):
+            print_err(line)
+    except TimeoutExpired:
+        proc.kill()
+        outs, _ = proc.communicate()
+        for line in outs.decode().split("\n"):
+            print_err(line)
