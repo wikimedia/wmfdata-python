bbcd20aca9f4316a67287f93b89c825a31e46825
Neil P. Quinn <nquinn@wikimedia.org>
2018-11-19 09:57:56 -0800

    Add missing comma

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.1",
     description="Tools for analyzing data on SWAP, a platform for confidential Wikimedia data",
     install_requires=["pandas", "impyla", "mysql-connector-python", "matplotlib"],
-    packages=find_packages()
+    packages=find_packages(),
     python_requires=">=3"
 )
