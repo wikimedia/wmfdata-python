97caf8c4e2cb42ed12d37a724c839ffa29a6560a
Neil Patel Quinn <nquinn@wikimedia.org>
2018-11-27 03:23:19 +0000

    Require matplotlib >= 2.1, which introduced ticker.PercentFormatter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="wmfdata",
     version="0.1",
     description="Tools for analyzing data on SWAP, a platform for confidential Wikimedia data",
-    install_requires=["pandas", "impyla", "mysql-connector-python", "matplotlib"],
+    install_requires=["pandas", "impyla", "mysql-connector-python", "matplotlib>=2.1"],
     packages=find_packages(),
     python_requires=">=3"
 )
