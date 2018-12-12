94aaa308985d4af1f5dd0acd1ef63960c53ac0f2
Neil P. Quinn <nquinn@wikimedia.org>
2018-12-12 15:00:57 -0800

    Add thrift-sasl dependency and reformat requires
    * Require thrift-sasl 0.2.1
    * Alphabetize requirements
    * Add comment explaining which we need matplotlib 2.1 or greater

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ setup(
     name="wmfdata",
     version="0.1",
     description="Tools for analyzing data on SWAP, a platform for confidential Wikimedia data",
-    install_requires=["pandas", "impyla", "mysql-connector-python", "matplotlib>=2.1"],
+    install_requires=[
+        "impyla",
+        "matplotlib>=2.1", # 2.1 introducted ticker.PercentFormatter
+        "mysql-connector-python", 
+        "pandas",
+        "thrift-sasl==0.2.1" # impyla can't connect properly to Hive with a later version
+    ],
     packages=find_packages(),
     python_requires=">=3"
 )
