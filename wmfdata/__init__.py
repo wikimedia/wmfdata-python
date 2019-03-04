98c10bf6aa097a5d0307ea8d4d1d26a986e6af45
Neil Patel Quinn <nquinn@wikimedia.org>
2019-03-04 17:37:51 +0000

    Unimport correct package

--- wmfdata/__init__.py
+++ wmfdata/__init__.py
@@ -1,5 +1,5 @@
 # Import all submodules so all are accessible after `import wmfdata`
-from wmfdata import charting, mariadb, utils # hive,
+from wmfdata import charting, hive, utils # mariadb,
 
 # Direct reusers to package
 utils.print_err("You can find the source for `wmfdata` at https://github.com/neilpquinn/wmfdata")
