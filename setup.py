1ff1fc1d93b42ea7f04760377d722fe5d19e293a
Mikhail Popov <mpopov@wikimedia.org>
2019-12-20 13:40:23 -0500

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="wmfdata",
-    version="0.1",
+    version="0.1.1",
     description="Tools for analyzing data on SWAP, a platform for confidential Wikimedia data",
     install_requires=[
         "impyla",
         "matplotlib>=2.1", # 2.1 introducted ticker.PercentFormatter
-        "mysql-connector-python", 
+        "mysql-connector-python",
         "pandas",
         "thrift-sasl==0.2.1" # impyla can't connect properly to Hive with a later version
     ],
