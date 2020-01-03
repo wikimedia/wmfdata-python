2b10ea56fbd04c6766a7ad037756412f9fdf4d02
Mikhail Popov <mpopov@wikimedia.org>
2020-01-03 11:18:07 -0500

    Move version out of setup.py

--- wmfdata/__init__.py
+++ wmfdata/__init__.py
@@ -1,5 +1,10 @@
-# Import all submodules so all are accessible after `import wmfdata`
-from wmfdata import charting, hive, utils # mariadb,
+__version__ = "0.1.0"
+__source__ = "https://github.com/neilpquinn/wmfdata"
 
-# Direct reusers to package
-utils.print_err("You can find the source for `wmfdata` at https://github.com/neilpquinn/wmfdata")
+# Import all submodules so all are accessible after `import wmfdata`
+from wmfdata import charting, hive, utils  # mariadb,
+
+welcome_message = "You are using wmfdata version {0}. You can find the source for `wmfdata` at {1}"
+welcome_message.format(__version__, __source__)
+
+utils.print_err(welcome_message)
