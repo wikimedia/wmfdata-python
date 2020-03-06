a2b5b8dec3b2ff200a144d106a3a4b434a05d190
Neil Patel Quinn <nquinn@wikimedia.org>
2020-03-06 18:43:29 +0000

    Point to changelog on release branch

--- wmfdata/__init__.py
+++ wmfdata/__init__.py
@@ -7,7 +7,7 @@ try:
         update_message = (
             "You are using wmfdata v{0}, but v{1} is available.\n\n" +
             "To update, run `pip install --upgrade git+{2}/wmfdata.git@release`.\n\n" +
-            "To see the changes, refer to {2}/blob/master/CHANGELOG.md"
+            "To see the changes, refer to {2}/blob/release/CHANGELOG.md"
         ).format(metadata.version, remote['version'], metadata.source)
         utils.print_err(update_message)
 
